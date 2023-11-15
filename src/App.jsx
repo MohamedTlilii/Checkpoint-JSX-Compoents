b3d534b5383636a03fa88b4c03acb5f33ed3495e
NAUT5 <Mohamedtlili1995@hotmail.com>
2023-11-15 09:26:30 +0100

    add

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,10 @@ import Price from "./Components/Price";
 import Name from "./Components/Name";
 
 function App() {
-  let data = "https://aws-tiqets-cdn.imgix.net/images/reviews/2b28413472424e4eb6755498984eb36c.jpg?auto=format&fit=crop&h=800&ixlib=python-3.2.1&q=70&w=800&s=a8e6a602e695586f5913b146116c45e6"
-  let firstName =false;
+  let data =
+    "https://aws-tiqets-cdn.imgix.net/images/reviews/2b28413472424e4eb6755498984eb36c.jpg?auto=format&fit=crop&h=800&ixlib=python-3.2.1&q=70&w=800&s=a8e6a602e695586f5913b146116c45e6";
+  let firstName = "mohamed";
+
   return (
     <div className="App">
       <Card style={{ width: "18rem" }}>
@@ -25,11 +27,10 @@ function App() {
           <Button variant="primary">
             <Price />
           </Button>
-          {firstName ? <h1></h1> : <img src={data}></img>}
         </Card.Body>
       </Card>
-      
-
+      {firstName ? <h1>Hello {firstName}</h1> : <h1>Hello there !</h1>}
+      {firstName && <img src={data} alt="" />}
     </div>
   );
 }
--- src/Components/Name.jsx
+++ src/Components/Name.jsx
@@ -1,10 +1,9 @@
 import Product from "../Product";
 function Name() {
-  let data = "Hello, there!";
-  let msg = false;
+  
   return (
     <div>
-      {msg ? <h2>Hello</h2> : <h2>{data}</h2>}
+      
       <p>{Product.name}</p>
     </div>
   );
